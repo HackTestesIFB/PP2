faed50e784f518fda6dd674007b4f3433b8cec00
HackTestesIFB <caio.flausino@estudante.ifb.edu.br>
2022-04-26 17:47:13 -0300

    Módulo 6 completo

--- experiencein/perfis/models.py
+++ experiencein/perfis/models.py
@@ -4,6 +4,14 @@ from django.db import models
 
 class Perfil(models.Model):
     nome = models.CharField(max_length=255, null=False)
-    email = models.CharField(max_length=255, null=False)     
+    email = models.CharField(max_length=255, null=False)
     telefone = models.CharField(max_length=15, null=False)
     nome_empresa = models.CharField(max_length=255, null=False)
+
+    def convidar(self, perfil_convidado):
+        Convite(solicitante=self, convidado=perfil_convidado).save()
+
+# logo abaixo da declaração da classe Perfil
+class Convite(models.Model):
+   solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
+   convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,6 +1,8 @@
 # experiencein/perfis/views.py 
 from django.shortcuts import render
 from perfis.models import Perfil
+from django.shortcuts import redirect
+
 
 def index(request):
     return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
@@ -8,3 +10,17 @@ def index(request):
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', { "perfil" : perfil})
+
+
+# modificando a função convidar
+def convidar(request, perfil_id):
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+
+    # realizando redirecionamento
+    return redirect('index')
+
+# ATENÇÃO, TROQUE O ID POR UM QUE ESTEJA CADASTRADO
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1) 
