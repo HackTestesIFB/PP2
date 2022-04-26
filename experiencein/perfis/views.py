08e727c9d3f8efbeb14b8c33358ddf57b5faa115
HackTestesIFB <caio.flausino@estudante.ifb.edu.br>
2022-04-25 23:14:55 -0300

    Módulo 4 completo

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -6,12 +6,5 @@ def index(request):
     return render(request, 'index.html')
 
 def exibir(request, perfil_id):
-
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
-    if perfil_id == 2:
-        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
-
-    return render(request, 'perfil.html', {'perfil' : perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
