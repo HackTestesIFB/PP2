5ab447071dd550f2941cdd81b67654e668fc0fec
HackTestesIFB <caio.flausino@estudante.ifb.edu.br>
2022-06-07 16:02:00 -0300

    Módulo 12 completo - sem as modificações do exercício

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -3,21 +3,26 @@ from django.shortcuts import render
 from perfis.models import *
 from django.shortcuts import redirect
 
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
+@login_required
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
     perfil_logado = get_perfil_logado(request)
     ja_e_contato = perfil in perfil_logado.contatos.all()
     return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 'ja_e_contato' : ja_e_contato})
 
+@login_required
 def aceitar(request, convite_id):
     convite = Convite.objects.get(id=convite_id)
     convite.aceitar()
     return redirect('index')
 
+@login_required
 # modificando a função convidar
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
@@ -27,6 +32,6 @@ def convidar(request, perfil_id):
     # realizando redirecionamento
     return redirect('index')
 
-# ATENÇÃO, TROQUE O ID POR UM QUE ESTEJA CADASTRADO
+@login_required
 def get_perfil_logado(request):
-    return Perfil.objects.get(id=1) 
+    return request.user.perfil
